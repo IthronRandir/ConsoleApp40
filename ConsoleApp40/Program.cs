dfc9ffd8fb84f99aa19aabb09eb643c0d8e5e62b
MSI <MSI@HOME-PC>
2023-04-11 18:50:00 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp40/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+int[] nums=new int[10] { 1, 2, 3, 4, 5, 6, 7, 15, 9, 10};
+int target = 9;
+int k1 = 0;
+int k2 = 0;
+do
+{
+    k2++;
+    if(k2==nums.Length)
+    {
+        k1++;
+        k2 = 0;
+    }
+}
+while (nums[k1] + nums[k2]!=target);
+Console.WriteLine($"[{k1};{k2}]");
